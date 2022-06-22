53a70a617e6ef0e17cd72962874808beaa461871
antara07 <chakrabortyantara07@gmail.com>
2022-06-22 17:02:09 +0530

    Changes for extract user from token

--- src/main/java/in/guidable/controllers/RoadmapController.java
+++ src/main/java/in/guidable/controllers/RoadmapController.java
@@ -1,14 +1,17 @@
 package in.guidable.controllers;
 
 import in.guidable.api.RoadmapsApi;
+import in.guidable.jwt.JwtUtil;
 import in.guidable.model.CreateRoadmapDetail;
 import in.guidable.model.CreateRoadmapResponse;
 import in.guidable.model.UpdateRoadmapDetail;
 import in.guidable.services.RoadmapService;
+import in.guidable.util.AuthenticationUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,16 +19,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RoadmapController implements RoadmapsApi {
 
+    private  final JwtUtil jwtUtil;
     private final RoadmapService roadmapService;
+    private  final AuthenticationUtil authUtil;
     @Override
     @PreAuthorize("hasAuthority('ROLE_USER')")
-    public ResponseEntity<CreateRoadmapResponse> createRoadmap(CreateRoadmapDetail createRoadmapDetail) {
-        return ResponseEntity.ok(roadmapService.createRoadmap(createRoadmapDetail));
+    public ResponseEntity<CreateRoadmapResponse> createRoadmap(CreateRoadmapDetail createRoadmapDetail, String token) {
+
+        String userName= authUtil.getUserFromToken(token);
+        return ResponseEntity.ok(roadmapService.createRoadmap(userName, createRoadmapDetail));
     }
 
     @Override
-    public ResponseEntity<List<CreateRoadmapResponse>> getRoadMaps() {
-        return ResponseEntity.ok(roadmapService.listRoadmap());
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public ResponseEntity<List<CreateRoadmapResponse>> getRoadMaps(String token) {
+        String userName= authUtil.getUserFromToken(token);
+         return ResponseEntity.ok(roadmapService.listRoadmap(userName));
+
     }
 
     @Override
