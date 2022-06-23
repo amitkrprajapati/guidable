00457741039e67bb5715688866b7dfad6cfa5bad
rivumitter <rivumitter@gmail.com>
2022-06-23 18:00:16 +0530

    securedEnabled= true added according to comments

--- src/main/java/in/guidable/config/SecurityConfig.java
+++ src/main/java/in/guidable/config/SecurityConfig.java
@@ -17,7 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled= true,prePostEnabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig{
 
