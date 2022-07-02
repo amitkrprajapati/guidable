bd64e2dd701abc12f5e88e5493d577fa4d9d4d9c
Arghya Mitra <rivumitter@gmail.com>
2022-07-02 22:47:35 +0530

    Helper util added with null removal from list method (#12)
    * Helper util added with null removal from list method
    
    * getCustomerIdFromToken method added

--- src/main/java/in/guidable/util/AuthenticationUtil.java
+++ src/main/java/in/guidable/util/AuthenticationUtil.java
@@ -4,19 +4,23 @@ package in.guidable.util;
 import in.guidable.exceptions.RenderableException;
 import in.guidable.exceptions.RenderableExceptionGenerator;
 import in.guidable.jwt.JwtUtil;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
 public class AuthenticationUtil {
+    public static final String CUSTOMER_ID = "customerId";
     private final JwtUtil jwtUtil;
     public String getUserFromToken(String token) throws RenderableException
     {
         try
         {
             if (token != null && token.startsWith("Bearer ")) {
-                token = token.substring(7);
+                token = getTokenFromHeader(token);
                 return jwtUtil.extractUsername(token);
             }
         } catch (Exception e)
@@ -24,5 +28,19 @@ public class AuthenticationUtil {
             throw RenderableExceptionGenerator.generateInvalidAuthTokenException();
         }
         throw RenderableExceptionGenerator.generateInvalidAuthTokenException();
+    }
+    /**
+     * Returns customerId as UUID
+     * Accepts authHeader
+     */
+    public UUID getCustomerIdFromToken(String authHeader){
+        final String token = getTokenFromHeader(authHeader);
+        Claims claims = jwtUtil.extractAllClaims(token);
+        return  UUID.fromString((String) claims.get(CUSTOMER_ID));
+    }
+
+    private String getTokenFromHeader(String token) {
+        token = token.substring(7);
+        return token;
     }
 }
---
+++ src/main/java/in/guidable/util/HelperUtil.java
@@ -0,0 +1,14 @@
+package in.guidable.util;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class HelperUtil {
+
+    private static <T> List<T> removeNull(List<T> arr) {
+
+        return arr.stream().filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+}
