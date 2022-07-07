cdfc36982b6e0e93f6cbc18ab0d1e537bc4d2056
amitkrprajapati <prajapatiamit76@gmail.com>
2022-07-07 19:18:49 +0530

    Addressed, review comments

--- src/test/java/in/guidable/services/RoadmapServiceTest.java
+++ src/test/java/in/guidable/services/RoadmapServiceTest.java
@@ -24,15 +24,12 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class RoadmapServiceTest {
   @Autowired private RoadmapService roadmapService;
